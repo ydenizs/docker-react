23c12c276db04d9a16f706a066d1a1a22616183b
Yağız Deniz Salcı <ydenizsalci@windowslive.com>
2022-04-08 18:09:07 +0300

    update message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there...
+          Changed code in feature branch
         </p>
       </header>
     </div>
